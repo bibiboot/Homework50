ba55922c83806401d0088856a0680ed5b97678e5
dan-boa <rehman.d.fazlur@gmail.com>
2014-04-03 01:22:41 -0700

    Phase 1 is over

--- bidder.c
+++ bidder.c
@@ -14,25 +14,37 @@
 #define CRED_FILE2 "bidderPass2.txt"
 
 char PORT[100] = "9001";
+char HOST[100] = "127.0.0.1";
 
-void get_cred(char *result){
-    FILE *fp = fopen(CRED_FILE1, "r");
-    char line[100];
-    char ty[100], uname[100], pwd[100], account[100];
+int ipaddr_client(struct sockaddr cli_addr, char **human_cli_addr){
+    /*Convert sockaddr to human string ip address*/
+    struct sockaddr_in *cli_addr_in = (struct sockaddr_in*)(&cli_addr);
+    *human_cli_addr = inet_ntoa(cli_addr_in->sin_addr);
+    return 0;
+}
+
+int get_cred(int btype, char *result){
+    FILE *fp;
+    if(btype==1)
+        fp = fopen(CRED_FILE1, "r");
+    else
+        fp = fopen(CRED_FILE2, "r");
+
+    char ty[100], uname[100], pwd[100], account[100], line[100];
     while(fgets(line, 100, fp)!=NULL){
         sscanf(line, "%[^ ] %[^ ] %[^ ] %s", ty, uname, pwd, account);
     }
-    //printf("%s\t%s\t%s\t%s", ty, uname, pwd, account);
     sprintf((result), "Login#%s %s %s", uname, pwd, account);
-    printf("Valus = %s\n", result);
+    /*PRINT*/
+    printf("Phase 1: Login request. User: %s password: %s Bank account: %s\n", uname, pwd, account);
+    return 0;
 }
 
-void start(){
+void start(int btype){
 
     struct addrinfo hints, *res;
     char buff[9000];
     char mesg[100];
-
 
     memset(&hints, 0, 100);
     hints.ai_family = AF_INET;
@@ -40,13 +52,18 @@ void start(){
 
     getaddrinfo("127.0.0.1", PORT, &hints, &res);
 
+    char *client_ip;
     int client_sock = socket(res->ai_family, res->ai_socktype, 0);
     connect(client_sock, res->ai_addr, res->ai_addrlen);
 
-    get_cred(mesg);
-    strcat(mesg, "X");
+    ipaddr_client(*(res->ai_addr), &client_ip);
+    struct sockaddr_in *cli_addr_in = (struct sockaddr_in*)(res->ai_addr);
 
-    printf("BIDDER: Send %s\n", mesg);
+    get_cred(btype, mesg);
+    //strcat(mesg, "X");
+
+    /*PRINT*/
+    printf("Phase 1: <Bidder#>%d has TCP port %d and IP address: %s \n", btype, cli_addr_in->sin_port, client_ip);
     if(send(client_sock, mesg, 100, 0) == -1){
         perror("Send\n");
     }
@@ -54,13 +71,14 @@ void start(){
     if(recv(client_sock, buff, 9000, 0) == -1){
         perror("Recv\n");
     }
-    printf("BIDDER: Recieved %s\n", buff);
+    /*PRINT*/
+    printf("Phase 1: Login request reply: %s.\n", buff);
 }
 
 int bidder1(){
     /*Bidder two run by parent process*/
     //printf("Parent\n");
-    start();
+    start(1);
 
     return 0;
 }
@@ -68,7 +86,7 @@ int bidder1(){
 int bidder2(){
     /*Bidder two run by child process*/
     //printf("Child\n");
-    start();
+    start(2);
     return 1;
 }
 
@@ -80,7 +98,7 @@ int main(int argc, char *argv[]){
     }
     else{
         /*Parent*/
-        //bidder1();
+        bidder1();
 
     }
     
--- seller.c
+++ seller.c
@@ -10,21 +10,85 @@
 #include <arpa/inet.h> 
 #include <sys/wait.h> 
 
+#define CRED_FILE1 "sellerPass1.txt"
+#define CRED_FILE2 "sellerPass2.txt"
+
+char PORT[100] = "9001";
+char HOST[100] = "127.0.0.1";
+
+int ipaddr_client(struct sockaddr cli_addr, char **human_cli_addr){
+    /*Convert sockaddr to human string ip address*/
+    struct sockaddr_in *cli_addr_in = (struct sockaddr_in*)(&cli_addr);
+    *human_cli_addr = inet_ntoa(cli_addr_in->sin_addr);
+    return 0;
+}
+
+int get_cred(int btype, char *result){
+    FILE *fp;
+    if(btype==1)
+        fp = fopen(CRED_FILE1, "r");
+    else
+        fp = fopen(CRED_FILE2, "r");
+
+    char ty[100], uname[100], pwd[100], account[100], line[100];
+    while(fgets(line, 100, fp)!=NULL){
+        sscanf(line, "%[^ ] %[^ ] %[^ ] %s", ty, uname, pwd, account);
+    }
+    sprintf((result), "Login#%s %s %s", uname, pwd, account);
+    /*PRINT*/
+    printf("Phase 1: Login request. User: %s password: %s Bank account: %s\n", uname, pwd, account);
+    return 0;
+}
+
+void start(int btype){
+
+    struct addrinfo hints, *res;
+    char buff[9000];
+    char mesg[100];
+
+    memset(&hints, 0, 100);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    getaddrinfo(HOST, PORT, &hints, &res);
+
+    char *client_ip;
+    int client_sock = socket(res->ai_family, res->ai_socktype, 0);
+    connect(client_sock, res->ai_addr, res->ai_addrlen);
+
+    ipaddr_client(*(res->ai_addr), &client_ip);
+    struct sockaddr_in *cli_addr_in = (struct sockaddr_in*)(res->ai_addr);
+
+    get_cred(btype, mesg);
+    //strcat(mesg, "X");
+
+    /*PRINT*/
+    printf("Phase 1: <Seller#>%d has TCP port %d and IP address: %s \n", btype, cli_addr_in->sin_port, client_ip);
+    if(send(client_sock, mesg, 100, 0) == -1){
+        perror("Send\n");
+    }
+
+    if(recv(client_sock, buff, 9000, 0) == -1){
+        perror("Recv\n");
+    }
+    /*PRINT*/
+    printf("Phase 1: Login request reply: %s.\n", buff);
+}
+
 int seller1(){
-    /*Seller two run by parent process*/
-    printf("Parent\n");
+    /*Bidder two run by parent process*/
+    //printf("Parent\n");
+    start(1);
 
     return 0;
 }
 
 int seller2(){
-    /*Seller two run by child process*/
-    printf("Child\n");
-
+    /*Bidder two run by child process*/
+    //printf("Child\n");
+    start(2);
     return 1;
 }
-
-
 
 int main(int argc, char *argv[]){
     int pid;
@@ -41,6 +105,5 @@ int main(int argc, char *argv[]){
     /*Wait for the child to over itself*/
     int status;
     waitpid(pid, &status, 0);
-
 }
 
